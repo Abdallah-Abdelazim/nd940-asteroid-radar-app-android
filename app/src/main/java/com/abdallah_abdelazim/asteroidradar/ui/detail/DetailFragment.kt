b172e873aab812f1b41bbe0ac9880a636375ed58
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2023-01-23 05:10:14 +0200

    Review `DetailFragment`

--- app/src/main/java/com/abdallah_abdelazim/asteroidradar/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/abdallah_abdelazim/asteroidradar/ui/detail/DetailFragment.kt
@@ -40,7 +40,7 @@ class DetailFragment : Fragment() {
 
     private fun displayAstronomicalUnitExplanationDialog() {
         val builder = AlertDialog.Builder(requireActivity())
-            .setMessage(getString(R.string.astronomica_unit_explanation))
+            .setMessage(getString(R.string.astronomical_unit_explanation))
             .setPositiveButton(android.R.string.ok, null)
         builder.create().show()
     }
